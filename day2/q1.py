acb938b62d8fa964c43413e710f0c0fd85dd4a2d
sravan1946 <miniclip1956@gmail.com>
2024-12-17 21:11:52 +0530

    working day4

--- day2/q1.py
+++ day2/q1.py
@@ -1,30 +1,38 @@
-from pathlib import Path
-
 from rich import print
+from pathlib import Path
 from tqdm import tqdm
 
 with open(Path(__file__).parent / "data.txt") as file:
     data = [list(map(int, line.strip().split())) for line in file]
 
-
 def is_asc(data: list[int]) -> bool:
-    return all(data[i] >= data[i + 1] for i in range(len(data) - 1))
-
+    ret = True
+    for i in range(0, len(data)-1):
+        if data[i] < data[i+1]:
+            ret = False
+            break
+    return ret
 
 def is_dec(data: list[int]) -> bool:
-    return all(data[i] <= data[i + 1] for i in range(len(data) - 1))
-
+    ret = True
+    for i in range(0, len(data)-1):
+        if data[i] > data[i+1]:
+            ret = False
+            break
+    return ret
 
 def is_safe(data: list[int]) -> bool:
     if not is_asc(data) and not is_dec(data):
         return False
-    return not any(
-        0 < abs(data[i] - data[i + 1]) > 3 or abs(data[i] - data[i + 1]) == 0
-        for i in range(len(data) - 1)
-    )
+    for i in range(0, len(data)-1):
+        if 0 < abs(data[i] - data[i+1]) > 3 or abs(data[i] - data[i+1]) == 0:
+            return False
+    return True
 
-
-count = sum(bool(is_safe(li)) for li in tqdm(data, desc="part 1"))
+count = 0
+for li in tqdm(data, desc="part 1"):
+    if is_safe(li):
+        count += 1
 print("part 1:", count)
 
 count = 0
--- day4/q1.py
+++ day4/q1.py
@@ -8,16 +8,15 @@ from tqdm import tqdm
 with open(Path(__file__).parent / "testdata.txt") as file:
     temp_data = file.read().split("\n")
     data = [list(line) for line in temp_data]
-for i in data:
-    print(i)
 
 h = 0
 ve = 0
-d = 0
-for line_no, line in enumerate(data):
+d1 = 0
+d2 = 0
+for line_no, line in tqdm(enumerate(data)):
     for index, v in enumerate(line):
         if index >= len(line) - 3:
-            break
+            continue
         if (
             v == "X"
             and line[index + 1] == "M"
@@ -33,7 +32,7 @@ for line_no, line in enumerate(data):
 
     for index, v in enumerate(line):
         if line_no >= len(data) - 3:
-            break
+            continue
         if (
             v == "X"
             and data[line_no + 1][index] == "M"
@@ -49,7 +48,7 @@ for line_no, line in enumerate(data):
 
     for index, v in enumerate(line):
         if index >= len(line) - 3 or line_no >= len(data) - 3:
-            break
+            continue
         if (
             v == "X"
             and data[line_no + 1][index + 1] == "M"
@@ -61,10 +60,11 @@ for line_no, line in enumerate(data):
             and data[line_no + 2][index + 2] == "M"
             and data[line_no + 3][index + 3] == "X"
         ):
-            d += 1
+            # print(v, data[line_no + 1][index + 1], data[line_no + 2][index + 2], data[line_no + 3][index + 3], line_no, index)
+            d1 += 1
 
     for index, v in enumerate(line):
-        if index <= 3 or line_no >= len(data) - 3:
+        if  line_no >= len(data) - 3 or index <= 2:
             continue
         # print(line_no, len(data), index, len(line))
         if (
@@ -78,16 +78,44 @@ for line_no, line in enumerate(data):
             and data[line_no + 2][index - 2] == "M"
             and data[line_no + 3][index - 3] == "X"
         ):
-            print(
-                v,
-                data[line_no + 1][index - 1],
-                data[line_no + 2][index - 2],
-                data[line_no + 3][index - 3],
-                line_no,
-                index,
-            )
-            d += 1
+            # print(
+            #     v,
+            #     data[line_no + 1][index - 1],
+            #     data[line_no + 2][index - 2],
+            #     data[line_no + 3][index - 3],
+            #     line_no,
+            #     index,
+            # )
+            d2 += 1
+
+
 print("h:", h)
 print("ve:", ve)
-print("d:", d)
-print("total: ", h + ve + d)
+print("d1:", d1)
+print("d2:", d2)
+print("total part1: ", h + ve + d1 + d2)
+
+
+mas = 0
+
+for line_no, line in tqdm(enumerate(data)):
+    for index, v in enumerate(line):
+        if (line_no == 0) or (index == 0) or (line_no == len(data) - 1) or (index == len(line) - 1):
+            print("skipping", line_no, index)
+            continue
+        if (line_no == 7):
+            print(line_no, index)
+        if (
+            v == "A"
+            and (
+                data[line_no - 1][index-1] == "M" and data[line_no + 1][index+1] == "S"
+                or data[line_no - 1][index-1] == "S" and data[line_no + 1][index+1] == "M"
+            )
+            and (
+                data[line_no - 1][index+1] == "M" and data[line_no + 1][index-1] == "S"
+                or data[line_no - 1][index+1] == "S" and data[line_no + 1][index-1] == "M"
+            )
+        ):
+            print("found", line_no, index)
+            mas += 1
+print("mas:", mas)
